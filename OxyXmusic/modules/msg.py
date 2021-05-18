3b8cca9d8c93343c7f15fe464fcee92c0cb33d8f
Your Daddy <71930942+OxyNotOp@users.noreply.github.com>
2021-05-18 16:34:55 +0530

    Update msg.py

--- OxyXmusic/modules/msg.py
+++ OxyXmusic/modules/msg.py
@@ -26,7 +26,7 @@ class Messages:
 
 ➼ {PROJECT_NAME} can play music in your group's voice chat as well as channel voice chats
 
-➼ Assistant name >> @{ASSISTANT_NAME}\n\nClick next for instructions**
+➼ Assistant name >> @{ASSISTANT_NAME}\n\n➼ Click next for instructions**
 """,
         f"""
 **Setting up**
