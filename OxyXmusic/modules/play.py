b1f92082022231b7a6ac605a197c632dd00654ce
Your Daddy <71930942+OxyNotOp@users.noreply.github.com>
2021-05-19 14:26:42 +0530

    Update play.py

--- OxyXmusic/modules/play.py
+++ OxyXmusic/modules/play.py
@@ -395,7 +395,7 @@ async def m_cb(b, cb):
             await cb.answer("Chat is not connected!", show_alert=True)
 
 
-@Client.on_message(command("ply") & other_filters)
+@Client.on_message(command("play") & other_filters)
 async def play(_, message: Message):
     global que
     lel = await message.reply("🔄 **Processing**")
